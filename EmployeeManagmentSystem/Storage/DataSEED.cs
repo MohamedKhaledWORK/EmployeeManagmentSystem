d0223ea51197ac2a349b160ae8fe348c4c32dc3b
MohamedKhaled <work.mohamedkhaled@gmail.com>
2026-07-17 19:53:40 +0300

    Done building storage and seeding small mock data

--- EmployeeManagmentSystem/Storage/DataSEED.cs
+++ EmployeeManagmentSystem/Storage/DataSEED.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EmployeeManagementSystem.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,50 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagementSystem.Storage
 {
-    internal class DataSEED
+    public static class DataSEED
     {
+        public static void SeedData()
+        {
+            var employeeStorage = new SystemStorage<Employee>("employees.json");
+            var departmentStorage = new SystemStorage<Department>("departments.json");
+
+            if (!departmentStorage.Load().Any())
+            {
+                var departments = new List<Department>
+                {
+                    new Department { Id = 1, Name = "IT" },
+                    new Department { Id = 2, Name = "HR" },
+                    new Department { Id = 3, Name = "Finance" }
+
+                };
+                departmentStorage.Save(departments);
+            }
+            if (!employeeStorage.Load().Any())
+            {
+                var departments = departmentStorage.Load().ToList();
+                var employees = new List<Employee> {
+                   new Employee
+                {
+                    Id = 1,
+                    Name = "John Doe",
+                    JobTitle = "Software Engineer",
+                    MonthlySalary = 60000
+                    ,Department = departments[0]
+                    ,HireDate = new DateOnly(2020, 1, 15),
+                    EmploymentStatus = EmployeeStatus.Active
+                },
+               new Employee
+                {
+                    Id = 2,
+                    Name = "Jane Smith",
+                    JobTitle = "Project Manager",
+                    MonthlySalary = 75000,
+                   Department = departments[1]
+                    ,HireDate = new DateOnly(2020, 1, 15),
+                    EmploymentStatus = EmployeeStatus.Active
+                } };
+                employeeStorage.Save(employees);
+            }
+        }
     }
 }
---
+++ EmployeeManagmentSystem/Storage/SystemStorage.cs
@@ -0,0 +1,44 @@
+﻿using EmployeeManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Storage
+{
+    public  class SystemStorage<T>
+    {
+        private readonly string _filePath;
+
+        public SystemStorage(string fileName)
+        {
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
+            string dataFolder = Path.Combine(basePath, "Storage");
+            Directory.CreateDirectory(dataFolder);           
+            _filePath = Path.Combine(dataFolder, fileName);  
+        }
+
+        public void Save(IEnumerable<T> assets)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+            var json = JsonSerializer.Serialize(assets, options);
+            File.WriteAllText(_filePath, json);
+            Console.WriteLine($"{typeof(T).Name}s saved.");
+        }
+
+        public IEnumerable<T> Load()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return Enumerable.Empty<T>();
+            }
+            string Json = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<IEnumerable<T>>(Json) ?? Enumerable.Empty<T>();
+        }
+    }
+}

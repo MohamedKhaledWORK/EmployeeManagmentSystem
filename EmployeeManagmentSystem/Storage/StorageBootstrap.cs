5301f4b7457d06bece066dede7b0dda03ec60324
MohamedKhaled <work.mohamedkhaled@gmail.com>
2026-07-18 11:45:10 +0300

    Done Adding Storage Resolver to determine which type of object we user

---
+++ EmployeeManagmentSystem/Storage/StorageBootstrap.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Storage
+{
+    public static class StorageBootstrap
+    {
+        private static StorageResolver? _Resolver;
+
+        public static StorageResolver CreateDefault()
+        {
+            if (_Resolver == null)
+            {
+                _Resolver = new StorageResolver();
+                _Resolver.Register<Employee>("employees.json");
+                _Resolver.Register<Department>("departments.json");
+            }
+            return _Resolver;
+
+        }
+    }
+}

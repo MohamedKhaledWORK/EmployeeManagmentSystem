84c4fd7b259682f06fb411e1c48ccb6f4222d2c1
MohamedKhaled <work.mohamedkhaled@gmail.com>
2026-07-18 11:39:38 +0300

    new exception type

---
+++ EmployeeManagmentSystem/Exceptions/TypeNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Exceptions
+{
+    public class TypeNotFoundException : Exception
+    {
+        public string Name { get; set; }
+        public TypeNotFoundException(string Name,string Message):base(Message)
+        {
+            this.Name = Name;
+        }
+    }
+}
---
+++ EmployeeManagmentSystem/Storage/StorageResolver.cs
@@ -0,0 +1,30 @@
+﻿using EmployeeManagementSystem.Exceptions;
+using EmployeeManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagementSystem.Storage
+{
+    public class StorageResolver
+    {
+        private readonly Dictionary<Type, string> _map = new();
+        public StorageResolver Register<T>(string fileName) where T : BaseClass 
+        {
+            _map[typeof(T)] = fileName;
+            return this;
+        }
+
+        public string Resolve<T>() where T : BaseClass
+        {
+            if(_map.TryGetValue(typeof(T), out var fileName))
+            {
+                return fileName;
+            }
+            throw new TypeNotFoundException(typeof(T).Name,$"No file registered");
+
+        }
+    }
+}
